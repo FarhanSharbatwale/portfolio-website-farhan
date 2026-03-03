d430434864d00536ca0c886ef1421411790f2802
Farhan Sharbatwale <farhan.csrit@gmail.com>
2026-03-03 17:52:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sendEmail() {
+    let emailBody = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+}
